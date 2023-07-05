e4f26aac4906fc5857de58967c7944336bd8da9b
catspie <catspie92@gmail.com>
2023-07-05 11:15:37 +0900

    "버튼 함수"

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,15 @@
-function goVegiterian(){
-    var v = f;
-    //버튼에 클릭 메서드를 등록합니다. 
-    $("button#vegOn").click(function(){
-       if(v == false){
-        v = true;
-       } 
-    }); //버튼 끝
-    $("button#restoreMe").click(function(){
-        if(v == true){
+
+//채식주의자 전용 메뉴판을 보여주는 vegOn 함수 시작
+$(function(){
+   var v = false;
+   $("button #vegOn").click(function(){
+      if(v == false){
+         v = true;
+      }   
+   });
+   $("button #restoreOn").click(function(){
+      if(v == true){
          v = false;
-        } 
-     }); //버튼 끝
-} //문서가 준비되었을 때 실행하는 함수 끝
+      }   
+   });
+});
