62fd0035d5690f625b9d4c5b8fd850e80fa0b0c0
catspie <catspie92@gmail.com>
2023-07-05 14:25:35 +0900

    "css 수정 및 detach()함수"

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,26 @@
 
-//채식주의자 전용 메뉴판을 보여주는 vegOn 함수 시작
+//채식주의자 전용 메뉴 호출 - 매뉴 되돌리기 기능
 $(function(){
    var v = false;
-   $("button #vegOn").click(function(){
+   $("button#vegOn").click(function(){
       if(v == false){
          v = true;
-      }   
-   });
-   $("button #restoreOn").click(function(){
+         //$(".meat").detach();
+         //id는 값을 하나만 가지고 올 수 있기 때문에 제일 먼저 선언된 text들로 채워진다. 
+         //$(".meat").after("<li class='tofu'>고기를 두부로 대체한 "+document.getElementById('meat').innerHTML+"</li>");
+         $(".fish").detach();
+         $(".hamburger").replaceWith("<li class='mushroom'>버섯튀김 (FRIED MUSHROOM)</li>");
+      $f = $(".fish").detach();
+      $m = $(".meat").detach();
+      $('.tofu').parent().addClass("veg_leaf");   
+   }   
+   });//버튼 끝
+   $("button#restoreMe").click(function(){
       if(v == true){
          v = false;
+         location.reload();
       }   
-   });
-});
+   });//버튼 끝
+});//함수 끝
+
+
